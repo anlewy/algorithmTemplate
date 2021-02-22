891a523e21896768f9221514147f854f67945a4a
wujiawen <wujiawen.wy@bytedance.com>
2021-02-22 16:45:01 +0800

    移除不需要提交到git部分

--- cmake-build-debug/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/admin/CLionProjects/FD/AlgorithmTemplate/googletest/googletest
-# Build directory: /Users/admin/CLionProjects/FD/AlgorithmTemplate/cmake-build-debug/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
